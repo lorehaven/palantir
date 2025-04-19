9a822e7c8b72adde92700dcad048ceb2e47d8210
Paweł Walus <pawel.walus@protonmail.com>
2025-04-19 20:26:24 +0200

    docker build fix

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build -t palantir .
+docker buildx build -t palantir .
 docker tag palantir ossiriand.arda:30021/ossiriand-1/palantir:1.0.0
 docker push ossiriand.arda:30021/ossiriand-1/palantir:1.0.0
